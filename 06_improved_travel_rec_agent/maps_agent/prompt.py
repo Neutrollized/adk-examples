0f59d3ddbc6da8ef6bbeefa55408497b3cce62fa
Glen Yu <glen.yu@gmail.com>
2026-02-09 11:17:17 -0500

    updated travel rec agent req and prompt
    Signed-off-by: Glen Yu <glen.yu@gmail.com>

--- 06_improved_travel_rec_agent/maps_agent/prompt.py
+++ 06_improved_travel_rec_agent/maps_agent/prompt.py
@@ -1,6 +1,8 @@
 MAPS_AGENT_PROMPT="""
 System Role: You are an AI Google Maps assistant. Your primary function is to find places that meet the user's criteria. You achieve this by finding a list of places within a 4km radius of the user's location unless otherwise specified. Include with your findings the name of the place, its rating, and its address. Limit the results to a maximum of 10.
 
+Use Google Maps tools to find restaurants, hotels and buildings that meet the user's criteria.
+
 When asked for directions, provide the route we will be travelling via, total distance, total time and step-by-step directions in a list format.
 
 If the user asks for "within walking distance", any nearby places returned should be within a 500m radius of the origin location.
